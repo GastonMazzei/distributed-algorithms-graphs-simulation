b0860b4d3717f25b228c917132307ed2daec8965
gaston <gastonmazzei95@gmail.com>
2021-09-28 15:12:27 +0200

    update!

--- Graph.py
+++ Graph.py
@@ -5,7 +5,7 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
-from aux import show_graph, build_tree
+from aux import show_graph_, build_tree_
 
 SEED = 1234
 np.random.seed(SEED)
@@ -22,18 +22,31 @@ class Graph():
         self.am = self.am.astype('uint8')
         self.i0 = None
         self.tree = None
+        self.tree_status = False
+        self.tree_length = 0
 
     def make_undirected():
         pass
-        
+
     def build_random_tree(self):
         self.i0 = np.random.randint(0,self.N)
-        self.tree = [[] for _ in range(self.N)]
+        self.tree = {_:{'parent':[],'children':[]} for _ in range(self.N)}
         build_tree_(self)
+        self.tree_status = True
 
     def view(self):
-        show_graph(self)
+        try:
+            show_graph_(self,3)
+        except:
+            try:
+                show_graph_(self,2)
+            except:
+                try:
+                    show_graph_(self,1)
+                except:
+                    show_graph_(self,0)
 
 if __name__=='__main__':
-    g = Graph(8,0.2)
+    g = Graph(8,0.5)
+    g.build_random_tree()
     g.view()
--- aux.py
+++ aux.py
@@ -5,9 +5,15 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
+DEBUG = True
 
-def show_graph(Graph):
+def show_graph_(Graph, layout_n=3):
 
+    if Graph.tree_status:
+        f, ax = plt.subplots(1,2,figsize=(12,6))
+    else:
+        f, ax = plt.subplots(1,1,figsize=(7,7))
+    
     # Get the adjcancency matrix from the Graph Class
     A = Graph.am
 
@@ -17,9 +23,9 @@ def show_graph(Graph):
     # Use some layout to handle positioning of graph
     LAYOUT = [nx.spring_layout,
             nx.spectral_layout,
-            nx.planar_layout,
             nx.shell_layout,
-            ][2]
+            nx.planar_layout,
+            ][layout_n]
     layout = LAYOUT(G)
 
     # Use a list for node_sizes
@@ -29,13 +35,114 @@ def show_graph(Graph):
     #color_map = ['g', 'b', 'r']
 
     # Draw the graph using the layout - with_labels=True if you want node labels.
-    nx.draw(G, layout, with_labels=True)#, node_size=sizes, node_color=color_map)
+    if Graph.tree_status:
+        nx.draw(G, layout, with_labels=True, ax=ax[0])
+        #, node_size=sizes, node_color=color_map)
+        # Get weights of each edge and assign to labels
+        labels = nx.get_edge_attributes(G, "weight")
+        # Draw edge labels using layout and list of labels
+        nx.draw_networkx_edge_labels(G, pos=layout, edge_labels=labels, ax=ax[0])
+    else:
+        nx.draw(G, layout, with_labels=True, ax=ax)
+        #, node_size=sizes, node_color=color_map)
+        # Get weights of each edge and assign to labels
+        labels = nx.get_edge_attributes(G, "weight")
+        # Draw edge labels using layout and list of labels
+        nx.draw_networkx_edge_labels(G, pos=layout, edge_labels=labels, ax=ax)
 
-    # Get weights of each edge and assign to labels
-    labels = nx.get_edge_attributes(G, "weight")
 
-    # Draw edge labels using layout and list of labels
-    nx.draw_networkx_edge_labels(G, pos=layout, edge_labels=labels)
+
+    if Graph.tree_status:
+
+        # Get the adjcancency matrix from the Graph's tree
+        A = tree_to_adjcacency_matrix(Graph.tree)
+        if DEBUG: print(f'A tree was made and it was this: {A}')
+        # Create DiGraph from A
+        G = nx.from_numpy_matrix(A, create_using=nx.DiGraph)
+
+        # Use a list for node_sizes
+        #sizes = [1000,400,200]
+
+        # Use a list for node colours
+        color_map = ['r' if _ != Graph.i0 else 'g' for _ in range(Graph.N)]
+        for i in range(len(color_map)):
+            if sum(A[i,:]+A[:,i])==0:
+                color_map[i] = 'w'
+
+        # Draw the graph using the layout - with_labels=True if you want node labels.
+        nx.draw(G, layout, 
+                    node_color=color_map,
+                    ax=ax[1]
+                    )
+
 
     # Show plot
     plt.show()
+
+def tree_to_adjcacency_matrix(tree):
+    k = sorted(list(tree.keys()))
+    N = len(k)
+    A = np.zeros((N,N))
+    for i in k:
+        for j in tree[i]['children']:
+            A[i,j] = 1
+    return A
+
+def build_tree_(Graph):
+    ixs = np.asarray(range(Graph.N))
+    marked = [False if _ != Graph.i0 else True for _ in range(Graph.N)]
+    parent = {}
+    children = {}
+    current = [Graph.i0]
+    current_children = []
+    tot = 1
+    def flatten(t):
+        return [item for sublist in t for item in sublist]
+    while sum(marked)!=Graph.N and tot != 0:
+        # Reinitialize counter of total news: it protects against disconnected nodes
+        tot = 0
+        dads = flatten(parent.values())
+        for i in current:
+            # Get the nodes that "i" points to
+            connected_to = ixs[np.where(Graph.am[i,:]!=0,True,False)].tolist()
+            # Exclude brothers, nephews and parents
+            connected_to = [_ for _ in connected_to if _ not in current + current_children + dads]
+            # For all the children:
+            for c in connected_to:
+                # Add them to the node's list of children
+                children[i] = children.get(i,[]) + [c]
+                # Add the node to the children's parent
+                parent[c] = [i]
+                # Mark the children as parsed
+                marked[c] = True
+                # Increase the counter by one
+                tot += 1
+            # Append the children connected to node "i" to the 
+            # total list of childrens at this level of the tree
+            current_children += connected_to.copy()
+        # The current nodes are those previously defined as children
+        current = current_children.copy()
+        # The list of current children gets reinitialized
+        current_children = []
+    # Store the results in the object
+    for i in range(Graph.N):
+        Graph.tree[i]['parent'] = parent.get(i,[])
+        Graph.tree[i]['children'] = children.get(i,[])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
